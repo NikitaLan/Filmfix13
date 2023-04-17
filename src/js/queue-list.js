bcf26a5335264eadda278f0abda9b48dabf0e55c
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-04-17 23:42:30 +0300

    render library card

--- src/js/queue-list.js
+++ src/js/queue-list.js
@@ -7,7 +7,7 @@ const refs = {
 const STORAGE_KEY = 'queue-list';
 let queueList = {};
 
-refs.watchedBtn.addEventListener('click', takeListFromStorage);
+// refs.watchedBtn.addEventListener('click', takeListFromStorage);
 
 function takeListFromStorage() {
   console.log('click on btn Queue');
