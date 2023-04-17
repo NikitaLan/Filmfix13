e9564a3065e396b3ef7e9919f1db4c859e575c67
ArtemOsypov <artem.osypov@gmail.com>
2023-04-17 23:40:43 +0300

    17042023 #1

--- src/js/makeGalleryMarkUpCard.js
+++ src/js/makeGalleryMarkUpCard.js
@@ -1,21 +1,21 @@
 import noPosterImg from '../images/noImage-placeholder.jpg';
-import {createGenresFromTrend} from '../js/genre/createGenresFromTrend'
+import { createGenresFromTrend } from '../js/genre/createGenresFromTrend';
 
 function createYear(release_date) {
-    return release_date ? release_date.split('-')[0] : '';
-  }
+  return release_date ? release_date.split('-')[0] : '';
+}
 
 export const makeGalleryMarkUp = films => {
-    return films
-      .map(
-        ({ genre_ids, release_date, vote_average, poster_path, title, id }) => {
-          const genre = createGenresFromTrend(genre_ids);
-          const filmYear = createYear(release_date);
-          const filmRate = vote_average.toFixed(1);
-          const poster = poster_path
-            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-            : noPosterImg;
-          return `
+  return films
+    .map(
+      ({ genre_ids, release_date, vote_average, poster_path, title, id }) => {
+        const genre = createGenresFromTrend(genre_ids);
+        const filmYear = createYear(release_date);
+        const filmRate = vote_average.toFixed(1);
+        const poster = poster_path
+          ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+          : noPosterImg;
+        return `
         <li class='gallery-home__item'>  
       <article class='gallery-home__card'>
         <a class='gallery-home__link'>
@@ -45,7 +45,53 @@ export const makeGalleryMarkUp = films => {
       </article>
     </li>
         `;
-        }
-      )
-      .join('');
-  }
+      }
+    )
+    .join('');
+};
+
+export const makeFilmMarkUp = ({
+  genre_ids,
+  release_date,
+  vote_average,
+  poster_path,
+  title,
+  id,
+}) => {
+  const genre = createGenresFromTrend(genre_ids);
+  const filmYear = createYear(release_date);
+  const filmRate = vote_average.toFixed(1);
+  const poster = poster_path
+    ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+    : noPosterImg;
+  return `
+        <li class='gallery-home__item'>  
+      <article class='gallery-home__card'>
+        <a class='gallery-home__link'>
+          <div class='gallery-home__card-thumb'>
+            <img
+              src='${poster}'
+              loading="lazy"
+              alt='${title}'
+              title='${title}'
+              data-id='${id}'
+              loading='lazy'
+              width='395'
+              height='500'
+            />
+            <span class="gallery-home__card-rate">${filmRate}</span>
+          </div>
+          <div class='gallery-home__card-info'>
+            <h3 class='gallery-home__card-title' data-id='${id}'>${title}</h3>
+  
+            <p class='gallery-home__card-text' data-id='${id}'>
+            ${genre}
+              |
+              ${filmYear}
+            </p>
+          </div>
+        </a>
+      </article>
+    </li>
+        `;
+};
--- src/js/whatched-list.js
+++ src/js/whatched-list.js
@@ -1,23 +1,78 @@
+// const refs = {
+//   watchedBtn: document.querySelector('.btn-watched'),
+//   watchedList: document.querySelector('.gallery-library__list'),
+//   textOoops: document.querySelector('.empty'),
+// };
+
+// const STORAGE_KEY = 'watched-list';
+// let watchedList = {};
+
+// refs.watchedBtn.addEventListener('click', takeListFromStorage);
+
+// function takeListFromStorage() {
+//   console.log('click on btn Watched');
+//   const listFromStorage = localStorage.getItem(STORAGE_KEY);
+//   watchedList = JSON.parse(listFromStorage);
+//   console.log(watchedList.results);
+//   refs.watchedList.insertAdjacentHTML(
+//     'afterbegin',
+//     cardMarkup(watchedList.results)
+//   );
+//   refs.textOoops.classList.add('visually-hidden');
+//   refs.watchedList.classList.remove('visually-hidden');
+// }
+
+import { makeFilmMarkUp } from '../js/makeGalleryMarkUpCard';
+
 const refs = {
   watchedBtn: document.querySelector('.btn-watched'),
   watchedList: document.querySelector('.gallery-library__list'),
   textOoops: document.querySelector('.empty'),
 };
 
-const STORAGE_KEY = 'watched-list';
-let watchedList = {};
+const STORAGE_KEY = 'watched';
+const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const API_KEY = '3dd9518c386fd347d5f1ac2580a699a4';
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+async function getMovieInfoById(movieID) {
+  const resp = await fetch(
+    `${BASE_URL}${movieID}?api_key=${API_KEY}&language=en-US`
+  );
+  const respData = await resp.json();
+
+  return respData;
+}
 
 refs.watchedBtn.addEventListener('click', takeListFromStorage);
 
 function takeListFromStorage() {
   console.log('click on btn Watched');
-  const listFromStorage = localStorage.getItem(STORAGE_KEY);
-  watchedList = JSON.parse(listFromStorage);
-  console.log(watchedList.results);
-  refs.watchedList.insertAdjacentHTML(
-    'afterbegin',
-    cardMarkup(watchedList.results)
-  );
+  const listFromStorage = load(STORAGE_KEY);
+  listFromStorage.map(el => {
+    getMovieInfoById(el).then(data => {
+      console.log(data);
+      return refs.watchedList.insertAdjacentHTML(
+        'beforeend',
+        makeFilmMarkUp(data)
+      );
+    });
+  });
+
+  // const listFromStorage = localStorage.getItem(STORAGE_KEY);
+  // watchedList = JSON.parse(listFromStorage);
+  // console.log(watchedList.results);
+  // refs.watchedList.insertAdjacentHTML(
+  //   'afterbegin',
+  //   cardMarkup(watchedList.results)
+  // );
   refs.textOoops.classList.add('visually-hidden');
   refs.watchedList.classList.remove('visually-hidden');
 }
